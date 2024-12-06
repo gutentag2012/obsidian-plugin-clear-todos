c962e5b441639df07f60376a9735f288d2dd9b48
Joshua Gawenda <joshua.gawenda@gmail.com>
2024-12-06 17:24:20 +0100

    feat: added editor and file menu items

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { Editor, MarkdownView, Plugin } from 'obsidian';
+import {Editor, MarkdownView, Menu, Plugin, TAbstractFile, TFile} from 'obsidian';
 
 const DONE_TODO_REGEX = /(^|\n)\t*- \[x\].*?(?=\n|$)/g;
 
@@ -7,14 +7,54 @@ export default class ClearTodosPlugin extends Plugin {
 		this.addCommand({
 			id: 'clear-todos',
 			name: 'Clear in selection or current file',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				const selection = editor.getSelection();
-				if(selection) {
-					editor.replaceSelection(selection.replace(DONE_TODO_REGEX, ''));
-					return
-				}
-				view.setViewData(view.data.replace(DONE_TODO_REGEX, ''), false);
-			}
+			editorCallback: (_editor: Editor, view: MarkdownView) => {
+				this.clearTodosOnView(view)
+			},
+			icon: "check-check"
 		});
+
+		this.app.workspace.on("editor-menu", (menu) => {
+			const view = this.app.workspace.getActiveViewOfType(MarkdownView)
+			if(!view) {
+				console.error("Clear Todos: No active MarkdownView found")
+				return
+			}
+			menu.addItem((item) =>
+				item
+					.setTitle("Clear in selection or current file")
+					.setIcon("check-check")
+					.onClick(async () => {
+						this.clearTodosOnView(view)
+					})
+			)
+		})
+		this.app.workspace.on("file-menu", (menu, file) => {
+			menu.addItem((item) =>
+				item
+					.setTitle("Clear Todos in file")
+					.setIcon("check-check")
+					.onClick(async () => {
+						await this.clearTodosForFile(file)
+					})
+			)
+		})
+	}
+
+	private async clearTodosForFile(file: TAbstractFile) {
+		if(!(file instanceof TFile)) return
+		const fileContent = await file.vault.read(file)
+		await file.vault.modify(file, this.clearTodoString(fileContent))
+	}
+
+	private clearTodosOnView(view: MarkdownView) {
+		const selection = view.editor?.getSelection();
+		if (selection) {
+			return view.editor.replaceSelection(this.clearTodoString(selection))
+		}
+		return view.setViewData(this.clearTodoString(view.data), false)
+	}
+
+	private clearTodoString(todoString: string) {
+		return todoString.replace(DONE_TODO_REGEX, "")
 	}
 }
