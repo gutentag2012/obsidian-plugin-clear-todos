dd0355109ad009df96843d6eca2f40b05fa36706
Joshua Gawenda <joshua.gawenda@gmail.com>
2024-12-22 17:36:16 +0100

    chore: move strings to constants

--- main.ts
+++ main.ts
@@ -1,6 +1,12 @@
 import {Editor, EventRef, MarkdownView, Plugin, TAbstractFile, TFile} from 'obsidian';
 
 const DONE_TODO_REGEX = /(^|\n)\t*- \[x\].*?(?=\n|$)/g;
+const TODO_TEXTS = {
+	clearSelection: "Clear todos in selection or current file",
+	clearFile: "Clear todos in file"
+}
+const CLEAR_TODO_ICON = "check-check"
+const CLEAR_COMMAND_ID = "clear-todos"
 
 export default class ClearTodosPlugin extends Plugin {
 	private editorMenuEvent: EventRef;
@@ -8,12 +14,12 @@ export default class ClearTodosPlugin extends Plugin {
 
 	async onload() {
 		this.addCommand({
-			id: 'clear-todos',
-			name: 'Clear todos in selection or current file',
+			id: CLEAR_COMMAND_ID,
+			name: TODO_TEXTS.clearSelection,
 			editorCallback: (_editor: Editor, view: MarkdownView) => {
 				this.clearTodosOnView(view)
 			},
-			icon: "check-check"
+			icon: CLEAR_TODO_ICON
 		});
 
 		this.editorMenuEvent = this.app.workspace.on("editor-menu", (menu) => {
@@ -24,28 +30,24 @@ export default class ClearTodosPlugin extends Plugin {
 			}
 			menu.addItem((item) =>
 				item
-					.setTitle("Clear todos in selection or current file")
-					.setIcon("check-check")
-					.onClick(async () => {
-						this.clearTodosOnView(view)
-					})
+					.setTitle(TODO_TEXTS.clearSelection)
+					.setIcon(CLEAR_TODO_ICON)
+					.onClick(() => this.clearTodosOnView(view))
 			)
 		})
 		this.fileMenuEvent = this.app.workspace.on("file-menu", (menu, file) => {
 			menu.addItem((item) =>
 				item
-					.setTitle("Clear todos in file")
-					.setIcon("check-check")
-					.onClick(async () => {
-						await this.clearTodosForFile(file)
-					})
+					.setTitle(TODO_TEXTS.clearFile)
+					.setIcon(CLEAR_TODO_ICON)
+					.onClick(() => this.clearTodosForFile(file))
 			)
 		})
 	}
 
 	onunload() {
 		super.onunload();
-		this.removeCommand('clear-todos');
+		this.removeCommand(CLEAR_COMMAND_ID);
 		this.app.workspace.offref(this.editorMenuEvent);
 		this.app.workspace.offref(this.fileMenuEvent);
 	}
